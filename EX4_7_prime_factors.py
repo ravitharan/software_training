6faa6f499e6c6561889875868b84cd3abadbf2f1
Ravi Ravitharan <ravi.ravitharan@wae.com>
2021-06-12 16:40:18 +0100

    Complete prime number factorization

--- EX4_7_prime_factors.py
+++ EX4_7_prime_factors.py
@@ -1,16 +1,6 @@
-# # EX:4 6) Create 100 random numbers between 1 to 100 and group every number into bins
-# with intervals of 10. Eg bins are 1-10, 11-20,..91-100.
+# EX:6 1) Find prime factors of a number.
 
 import sys
-
-def check_prime(number): 
-    is_prime = True
-
-    for i in range(2, number):
-        if (number % i) == 0:
-            is_prime = False
-            break
-    return is_prime
 
 if len(sys.argv) != 2:
     print(f"Argument error\n Usage {sys.argv[0]} <number>")
@@ -19,9 +9,11 @@ if len(sys.argv) != 2:
 number = int(sys.argv[1])
 
 factors = []
-while number > 1:
-    for prime in range(2, number):
+factor = 2
+while (number >= factor):
+    while ((number % factor) == 0):
+        factors.append(factor)
+        number = number // factor
+    factor += 1
 
-
-
-
+print(factors)
