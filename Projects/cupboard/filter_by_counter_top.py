c58c8467ce27f83d6e7f8ac647883621dc7201e1
BaskaranTharsan <89092348+BaskaranTharsan@users.noreply.github.com>
2021-11-08 13:20:58 +0530

    Update filter_by_counter_top.py

--- Projects/cupboard/filter_by_counter_top.py
+++ Projects/cupboard/filter_by_counter_top.py
@@ -27,31 +27,43 @@ if __name__ == "__main__":
     template_sheet["AC30"] = active_sheet["AC30"].value
     template_sheet["AG1"] = active_sheet["AG1"].value
     template_sheet["AG30"] = active_sheet["AG30"].value
+    row_num = []
+    i = 6
+    add = 1
+    while i < maximum_row:     
+        if add % 9 != 0:
+            row_num.append(i)    
+            i += 3
+        else:
+            i += 5
+        add += 1
+
+
     for row in range(6 , maximum_row+1):
         if active_sheet["AD" + str(row)].value != None:
             if "COMPANY" in active_sheet[get_column_letter(1) + str(row)].value:
-                print(row)
-                template_sheet[get_column_letter(1) + str(row)] = active_sheet[get_column_letter(1) + str(row)].value
-                template_sheet[get_column_letter(1) + str(row+2)] = active_sheet[get_column_letter(1) + str(row+2)].value
-                template_sheet[get_column_letter(2) + str(row+2)] = active_sheet[get_column_letter(2) + str(row+2)].value
+                template_sheet[get_column_letter(1) + str(row_num[0])] = active_sheet[get_column_letter(1) + str(row)].value
+                template_sheet[get_column_letter(1) + str(row_num[0]+2)] = active_sheet[get_column_letter(1) + str(row+2)].value
+                template_sheet[get_column_letter(2) + str(row_num[0]+2)] = active_sheet[get_column_letter(2) + str(row+2)].value
                 for col in range(3,10):
-                    template_sheet[get_column_letter(col) + str(row)] = active_sheet[get_column_letter(col) + str(row)].value
+                    template_sheet[get_column_letter(col) + str(row_num[0])] = active_sheet[get_column_letter(col) + str(row)].value
                 for col in range(10,12):
-                    template_sheet[get_column_letter(col) + str(row)] = active_sheet[get_column_letter(col) + str(row)].value
-                    template_sheet[get_column_letter(col) + str(row+2)] = active_sheet[get_column_letter(col) + str(row+2)].value
-                template_sheet[get_column_letter(12) + str(row)] = active_sheet[get_column_letter(12) + str(row)].value
-                template_sheet[get_column_letter(12) + str(row+1)] = active_sheet[get_column_letter(12) + str(row+1)].value
-                template_sheet[get_column_letter(12) + str(row+2)] = active_sheet[get_column_letter(12) + str(row+2)].value
-                template_sheet[get_column_letter(13) + str(row)] = active_sheet[get_column_letter(13) + str(row)].value
-                template_sheet[get_column_letter(13) + str(row+2)] = active_sheet[get_column_letter(13) + str(row+2)].value
+                    template_sheet[get_column_letter(col) + str(row_num[0])] = active_sheet[get_column_letter(col) + str(row)].value
+                    template_sheet[get_column_letter(col) + str(row_num[0]+2)] = active_sheet[get_column_letter(col) + str(row+2)].value
+                template_sheet[get_column_letter(12) + str(row_num[0])] = active_sheet[get_column_letter(12) + str(row)].value
+                template_sheet[get_column_letter(12) + str(row_num[0]+1)] = active_sheet[get_column_letter(12) + str(row+1)].value
+                template_sheet[get_column_letter(12) + str(row_num[0]+2)] = active_sheet[get_column_letter(12) + str(row+2)].value
+                template_sheet[get_column_letter(13) + str(row_num[0])] = active_sheet[get_column_letter(13) + str(row)].value
+                template_sheet[get_column_letter(13) + str(row_num[0]+2)] = active_sheet[get_column_letter(13) + str(row+2)].value
                 for col in range(14,27):
-                    template_sheet[get_column_letter(col) + str(row)] = active_sheet[get_column_letter(col) + str(row)].value
-                template_sheet[get_column_letter(27) + str(row)] = active_sheet[get_column_letter(27) + str(row)].value
-                template_sheet[get_column_letter(28) + str(row)] = active_sheet[get_column_letter(28) + str(row)].value
-                template_sheet[get_column_letter(28) + str(row+2)] = active_sheet[get_column_letter(28) + str(row+2)].value
-                template_sheet[get_column_letter(29) + str(row+2)] = active_sheet[get_column_letter(29) + str(row+2)].value
+                    template_sheet[get_column_letter(col) + str(row_num[0])] = active_sheet[get_column_letter(col) + str(row)].value
+                template_sheet[get_column_letter(27) + str(row_num[0])] = active_sheet[get_column_letter(27) + str(row)].value
+                template_sheet[get_column_letter(28) + str(row_num[0])] = active_sheet[get_column_letter(28) + str(row)].value
+                template_sheet[get_column_letter(28) + str(row_num[0]+2)] = active_sheet[get_column_letter(28) + str(row+2)].value
+                template_sheet[get_column_letter(29) + str(row_num[0]+2)] = active_sheet[get_column_letter(29) + str(row+2)].value
                 for col in range(30,41):
-                    template_sheet[get_column_letter(col) + str(row)] = active_sheet[get_column_letter(col) + str(row)].value
+                    template_sheet[get_column_letter(col) + str(row_num[0])] = active_sheet[get_column_letter(col) + str(row)].value
+                del row_num[0]
 
     template.save("ct_output.xlsx")      
         
