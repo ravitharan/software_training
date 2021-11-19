bc82555f665debb9c52c13cd8122799ca69ed102
Ravi Ravitharan <ravitharan@gmail.com>
2021-11-19 22:27:52 +0000

    Clean up hardware details

--- Projects/cupboard/Hardware_Details.py
+++ Projects/cupboard/Hardware_Details.py
@@ -4,11 +4,12 @@ import openpyxl
 from openpyxl.styles import *
 from openpyxl.utils import get_column_letter
 
-CUPBOARD_ID_COLUMN = "E"
-HARDWARE_ITEM_COLUMN = "L"
+SHEET_KNOB_COUNT        = "KNOB COUNT"
+CUPBOARD_ID_COLUMN      = "E"
+HARDWARE_ITEM_COLUMN    = "L"
 
-def hardware_data(excel_file):
-    current_sheet = excel_file["Main Sheet"] # Active VANITY INFO sheet
+def hardware_data(wb):
+    current_sheet = wb["Main Sheet"]
     maximum_row = current_sheet.max_row
     hardware_count_items = []
     for x in range(1 , maximum_row + 1):
@@ -18,90 +19,71 @@ def hardware_data(excel_file):
             c = x + 2
             hardware_count  = current_sheet[HARDWARE_ITEM_COLUMN + str(c)].value
             if hardware_count != None:
-                hardware_count = re.findall('[0-9]+', hardware_count)
+                hardware_count = re.findall('\d+', hardware_count)
                 for count in hardware_count:
                     count_items.append(int(count))
                     
-                hardware_item1 = current_sheet[HARDWARE_ITEM_COLUMN + str(x)].value
-                hardware_item2 = current_sheet[HARDWARE_ITEM_COLUMN + str(x+1)].value
-                if 'DR-' in hardware_item1:
-                    hardware_item1 = hardware_item1.replace('DR-','')
-                elif 'DW-' in hardware_item1:
-                    hardware_item1 = hardware_item1.replace('DW-','')
-                count_items.append(hardware_item1)
-                
-                if hardware_item2 != None:   
-                    if 'DR-' in hardware_item2 :
-                        hardware_item2 = hardware_item2.replace('DR-','')
-                         
-                    elif 'DW-' in hardware_item2:
-                        hardware_item2 = hardware_item2.replace('DW-','')
-                 
-                if  hardware_item2!= None and hardware_item2 != hardware_item1 :
-                    count_items.append(hardware_item2)
-            
-                hardware_count_items.append(count_items) 
+                hardware_door = current_sheet[HARDWARE_ITEM_COLUMN + str(x)].value
+                #Remove "DR-"
+                if hardware_door:
+                    hardware_door = hardware_door[3:]
+
+                hardware_drawer = current_sheet[HARDWARE_ITEM_COLUMN + str(x+1)].value
+                #Remove "DW-"
+                if hardware_drawer:
+                    hardware_drawer = hardware_drawer[3:]
+
+                hardware_count_items.append([count_items[0], hardware_door]) 
+                if len(count_items) == 2:
+                    hardware_count_items.append([count_items[1], hardware_drawer]) 
                 
     return hardware_count_items
  
 def hardware_add_count(hardware_count_item):
     hardware_item_set = set()
     for item_list in hardware_count_item:
-        if len(item_list) == 4:
-            hardware_item_set.add(item_list[2])
-            hardware_item_set.add(item_list[3])
-        else:
-            hardware_item_set.add(item_list[1])
+        hardware_item_set.add(item_list[1])
 
     hardware_count_Dict = {}
     for set_item in hardware_item_set:
         total = 0
         for list_item in hardware_count_item:
-            if len(list_item)== 4:
-                if list_item[2] == set_item:
-                    total += list_item[0]
-                if list_item[3] == set_item:
-                    total += list_item[1]
-            else:
-                if list_item[1] == set_item:
-                    total += list_item[0]
-                    
+            if list_item[1] == set_item:
+                total += list_item[0]
         hardware_count_Dict[set_item] = total
     
     return hardware_count_Dict
 
-def Hardware_Details_Write(hardware_count_Dict_data):
-    column_number1, column_number2 = 1, 2
-    column = str(chr(64 + column_number1))
-    sheet.column_dimensions[column].width = 24
-    column = str(chr(64 + column_number2))
-    sheet.column_dimensions[column].width = 24
+def Hardware_Details_Write(ws, start_row, hardware_count_Dict_data):
+    ws.column_dimensions['A'].width = 24
+    ws.column_dimensions['B'].width = 24
     
-    sheet['A1'] = 'Hardware Item' # Excel_cell
-    sheet['A1'].font = Font(bold=True, size = 16)
-    sheet['A1'].fill = PatternFill(fgColor="61E43A", fill_type = "solid")
-    sheet['A1'].alignment = Alignment(horizontal='center')
+    row = start_row
+    ws.cell(row, 1).value = 'Hardware Item' # Excel_cell
+    ws.cell(row, 1).font = Font(bold=True, size = 16)
+#    ws.cell(row, 1).fill = PatternFill(fgColor="61E43A", fill_type = "solid")
+    ws.cell(row, 1).alignment = Alignment(horizontal='center')
     
-    sheet['B1'] = 'Count' # Excel_cell
-    sheet['B1'].font = Font(bold=True, size = 16)
-    sheet['B1'].fill = PatternFill(fgColor="61E43A", fill_type = "solid")
-    sheet['B1'].alignment = Alignment(horizontal='center')
+    ws.cell(row, 2).value = 'Count' # Excel_cell
+    ws.cell(row, 2).font = Font(bold=True, size = 16)
+#    ws.cell(row, 2).fill = PatternFill(fgColor="61E43A", fill_type = "solid")
+    ws.cell(row, 2).alignment = Alignment(horizontal='center')
 
-    row = 2
+    row += 1
     total = 0
     for key in hardware_count_Dict_data:
-        sheet["A" + str(row)] = key # Excel_cell_value_add
-        sheet["B" + str(row)] = hardware_count_Dict_data[key] # Excel_cell_value_add
-        sheet["B"+ str(row)].alignment = Alignment(horizontal='center')
-        total += sheet["B" + str(row)].value
+        ws["A" + str(row)] = key # Excel_cell_value_add
+        ws["B" + str(row)] = hardware_count_Dict_data[key] # Excel_cell_value_add
+        ws["B"+ str(row)].alignment = Alignment(horizontal='center')
+        total += ws["B" + str(row)].value
         row += 1
     
-    sheet["A" + str(row+1)] = "Total"
-    sheet["A" + str(row+1)].font = Font(bold=True, size = 12)
+    ws["A" + str(row+1)] = "Total"
+    ws["A" + str(row+1)].font = Font(bold=True, size = 12)
     
-    sheet["B" + str(row+1)] = total
-    sheet["B" + str(row+1)].font = Font(bold=True, size = 12)
-    sheet["B"+ str(row+1)].alignment = Alignment(horizontal='center')
+    ws["B" + str(row+1)] = total
+    ws["B" + str(row+1)].font = Font(bold=True, size = 12)
+    ws["B"+ str(row+1)].alignment = Alignment(horizontal='center')
     
     return row
 
@@ -111,13 +93,14 @@ if __name__ == "__main__":
         exit(1)
 
     excel_file = sys.argv[1]    
-    excel_file = openpyxl.load_workbook(excel_file, data_only=True)
-    hardware_count_items = hardware_data(excel_file)
+    wb = openpyxl.load_workbook(excel_file, data_only=True)
+    hardware_count_items = hardware_data(wb)
     hardware_count_Dict = hardware_add_count(hardware_count_items)   
 
     excel_file = openpyxl.Workbook()
     sheet = excel_file.active
+    sheet.title = SHEET_KNOB_COUNT
     #sheet.title = "Hardware_Details"
-    Hardware_Details_Write(hardware_count_Dict)
+    Hardware_Details_Write(sheet, 3, hardware_count_Dict)
 
-    excel_file.save('Hardware Item Details.xlsx') # New Excel file create
+    excel_file.save('Hardware_Details.xlsx') # New Excel file create
