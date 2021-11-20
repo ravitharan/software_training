d49870a49c51e7cff6d688d5665973d5bda84e0a
Ravi Ravitharan <ravitharan@gmail.com>
2021-11-20 15:00:29 +0000

    Add big label into GUI

--- Projects/cupboard/vanity_parts_gui.py
+++ Projects/cupboard/vanity_parts_gui.py
@@ -5,6 +5,10 @@ from parse_order import *
 from write_order_parts import *
 from counter_top import *
 from Hardware_Details import *
+from label import *
+
+OUTPUT_FILE     = "output.xlsx"
+BIG_LABEL_FILE  = "big_label.xlsx"
 
 if __name__ == '__main__':
 
@@ -18,7 +22,7 @@ if __name__ == '__main__':
     ]
 
     # Create the Window
-    window = sg.Window('Vanity parts order v2.0', layout)
+    window = sg.Window('Vanity parts order v3.0', layout)
 
     # Event Loop to process "events" and get the "values" of the inputs
     return_value = "cancel"
@@ -27,7 +31,7 @@ if __name__ == '__main__':
         if event == 'Ok':
             input_xlsx = values[0]
             dst_path = os.path.dirname(input_xlsx)
-            dst_path = os.path.join(dst_path, "output.xlsx")
+            output_file = os.path.join(dst_path, OUTPUT_FILE)
 
             wb_in = openpyxl.load_workbook(input_xlsx, data_only=True)
             cupboard_parts = get_cupboard_list(wb_in)
@@ -60,6 +64,8 @@ if __name__ == '__main__':
             Hardware_Details_Write(ws_knob, START_ROW, knob_details)
             work_sheets.append(ws_knob)
 
+            wb_label_out = write_big_labels(wb_in)
+
             for ws in work_sheets:
                 ws.cell(1, 1).value = "WEEK NO:"
                 ws.cell(1, 1).font = Font(bold=True)
@@ -68,7 +74,9 @@ if __name__ == '__main__':
                 ws.cell(1, 2).font = Font(bold=True)
                 ws.cell(1, 2).alignment = Alignment(horizontal="center")
 
-            wb_out.save(dst_path)
+            wb_out.save(output_file)
+            label_file = os.path.join(dst_path, BIG_LABEL_FILE)
+            wb_label_out.save(label_file)
             return_value = "pass"
             break
         elif event == 'Cancel' or event == sg.WIN_CLOSED: # if user closes window or clicks cancel
@@ -77,4 +85,4 @@ if __name__ == '__main__':
     window.close()
 
     if return_value == "pass":
-        sg.popup(f'\n  Output saved into {dst_path}!  \n', title="output file")
+        sg.popup(f'\nOutput saved into {dst_path}  \nFiles:\n{OUTPUT_FILE}\n{BIG_LABEL_FILE} \n', title="output file")
