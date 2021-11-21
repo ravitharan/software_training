08eef2e2aea98a25f344df54d783a0bab01d3062
BaskaranTharsan <89092348+BaskaranTharsan@users.noreply.github.com>
2021-11-21 11:36:05 +0530

    New calculation for filter by counter top

--- Projects/cupboard/filter_by_counter_top.py
+++ Projects/cupboard/filter_by_counter_top.py
@@ -4,16 +4,10 @@ import sys
 
 def xl_filter(user_xl_file, template):
     active_sheet = user_xl_file["Main Sheet"]
-    template_sheet = template["Sheet1"]
+    template_sheet = template["Main Sheet"]
     maximum_row = active_sheet.max_row
     maximum_column = active_sheet.max_column
-    """Head write"""
-    counter_cell_map = ['C1', 'C30', 'H1', 'H30', 'X1', 'X30', 'AC1', 'AC30', 'AG1', 'AG30']
- 
-    for cell in counter_cell_map:
-        template_sheet[cell] = active_sheet[cell].value
 
-    """Write counter top order details"""
     row_num = []
     i = 6
     add = 1
@@ -25,32 +19,26 @@ def xl_filter(user_xl_file, template):
             i += 5
         add += 1
 
+    for row in range(1, maximum_row + 1):
+        """Header write"""
+        if isinstance(active_sheet.cell(row, 1).value, str):
+            if  "DATE ISSUE:" in active_sheet.cell(row, 1).value:
+                column_num = [3, 8, 12, 13, 15, 21, 24, 33, 40]
+                for column in column_num:
+                    if column != 40:
+                        template_sheet.cell(row, column).value = active_sheet.cell(row, column).value
+                    else:
+                        template_sheet.cell(row+1, column).value = active_sheet.cell(row+1, column).value
+        """Write counter top existing row"""
+        if active_sheet.cell(row, 30).value != None and isinstance(active_sheet.cell(row, 5).value, int):
+            row_memory = row_num[0]
+            for row_read in range(row , row+3):
+                for column in range(1, maximum_column+1):
+                    if active_sheet.cell(row_read, column).value != None:
+                        template_sheet.cell(row_memory, column).value = active_sheet.cell(row_read, column).value
+                row_memory += 1
 
-    for row in range(6 , maximum_row+1):
-        if active_sheet["AD" + str(row)].value != None:
-            if "COMPANY" in active_sheet[get_column_letter(1) + str(row)].value:
-                template_sheet[get_column_letter(1) + str(row_num[0])] = active_sheet[get_column_letter(1) + str(row)].value
-                template_sheet[get_column_letter(1) + str(row_num[0]+2)] = active_sheet[get_column_letter(1) + str(row+2)].value
-                template_sheet[get_column_letter(2) + str(row_num[0]+2)] = active_sheet[get_column_letter(2) + str(row+2)].value
-                for col in range(3,10):
-                    template_sheet[get_column_letter(col) + str(row_num[0])] = active_sheet[get_column_letter(col) + str(row)].value
-                for col in range(10,12):
-                    template_sheet[get_column_letter(col) + str(row_num[0])] = active_sheet[get_column_letter(col) + str(row)].value
-                    template_sheet[get_column_letter(col) + str(row_num[0]+2)] = active_sheet[get_column_letter(col) + str(row+2)].value
-                template_sheet[get_column_letter(12) + str(row_num[0])] = active_sheet[get_column_letter(12) + str(row)].value
-                template_sheet[get_column_letter(12) + str(row_num[0]+1)] = active_sheet[get_column_letter(12) + str(row+1)].value
-                template_sheet[get_column_letter(12) + str(row_num[0]+2)] = active_sheet[get_column_letter(12) + str(row+2)].value
-                template_sheet[get_column_letter(13) + str(row_num[0])] = active_sheet[get_column_letter(13) + str(row)].value
-                template_sheet[get_column_letter(13) + str(row_num[0]+2)] = active_sheet[get_column_letter(13) + str(row+2)].value
-                for col in range(14,27):
-                    template_sheet[get_column_letter(col) + str(row_num[0])] = active_sheet[get_column_letter(col) + str(row)].value
-                template_sheet[get_column_letter(27) + str(row_num[0])] = active_sheet[get_column_letter(27) + str(row)].value
-                template_sheet[get_column_letter(28) + str(row_num[0])] = active_sheet[get_column_letter(28) + str(row)].value
-                template_sheet[get_column_letter(28) + str(row_num[0]+2)] = active_sheet[get_column_letter(28) + str(row+2)].value
-                template_sheet[get_column_letter(29) + str(row_num[0]+2)] = active_sheet[get_column_letter(29) + str(row+2)].value
-                for col in range(30,41):
-                    template_sheet[get_column_letter(col) + str(row_num[0])] = active_sheet[get_column_letter(col) + str(row)].value
-                del row_num[0]
+            del row_num[0]
     template.save("ct_template.xlsx")
 
 if __name__ == "__main__":
