55dcbde1c0b278acf22115c741f9636718bc309e
Baskaran Tharsan <Tharshanbasss@gmail.com>
2021-12-12 16:33:25 +0530

    add counter top filter code

--- Projects/cupboard/label.py
+++ Projects/cupboard/label.py
@@ -208,6 +208,72 @@ def valance_data_write(wb_r, ws, valances):
             valance_code  = ws_r["J" + str(row_order+2)].value
             valance_val = valances[valance_code]
             ws[dst_cell].value = valance_val
+
+def counter_top_data_write(wb_r, ws_template):
+    """
+    Main sheet data write in to tha Label tamplate
+    
+    """
+    ws_r = wb_r["Main Sheet"]
+    ws_w = ws_template
+    label_count = 0
+    # row_label is as 1, 18, 33, 50, 65, 82, 97, 114, 129, 146, 161, 178, 193, 210
+    row_label = 0
+    counter_top_label_cell_map = [
+                            ('AI1', 'AD6'),
+                            ('AI2', 'B8'),
+                            ('AI4', 'D6'),
+                            ('AI5', 'H1'),
+                            ('AB7', 'A6'),
+                            ('AG9','AG3'),
+                            ('AC9', 'AF6'),
+                            ('AH9','AG6'),
+                            ('AB10','Y6'),
+                            ('AC10','Y3'),
+                            ('AG10','AH3'),
+                            ('AH10','AH6'),
+                            ('AH11','AI6'),
+                            ('AG11','AI3'),
+                            ('AH12','AK6'),
+                            ('AG12','AK3'),
+                            ('AG13','AJ3'),
+                            ('AH13','AJ6'),
+                            ('AC13','AA6')
+                            ]
+
+    for row_order in range(1, ws_r.max_row+1):
+        cupboard_id  = ws_r["E" + str(row_order)].value
+        order_number  = ws_r["AD" + str(row_order)].value  
+        if isinstance(cupboard_id, int) and order_number != None:
+            label_count += 1
+            if (label_count == 1):
+                row_label = 1
+            elif ((label_count % 2) == 0):
+                row_label += 17
+            else:
+                row_label += 15
+                
+            #image insertion
+            img = drawing.image.Image('logo_small.png')
+                #img.height=130
+                #img.width=200
+            img.anchor = 'AB'+str(1+row_label)
+            ws_w.add_image(img)
+            
+            add_list =[('AC10','Y3'),('AI5', 'H1'),('AG9','AG3'),('AG10','AH3'),('AG11','AI3'),('AG12','AK3'),('AG13','AJ3')]
+            for (dst, src) in counter_top_label_cell_map:
+                for (r,w) in add_list:
+                    if dst == r:
+                        dst_cell = add_row(dst, row_label - 1)
+                        src_cell = ws_r[w].value
+                        ws_w[dst_cell].value = src_cell
+                        break
+                 
+                    else:
+                        dst_cell = add_row(dst, row_label - 1)
+                        src_cell = add_row(src, row_order - 6)
+                        #print(f'dst {dst_cell}, src {src_cell}')
+                        ws_w[dst_cell].value = ws_r[src_cell].value
      
 def write_big_labels(wb):
     wb_template = load_workbook(BIG_LABEL_TEMPLATE)
@@ -219,7 +285,7 @@ def write_big_labels(wb):
     framed_mirror_data_write(wb, ws_template)
     valances = get_vanity_info_valance_detail(wb)
     valance_data_write(wb, ws_template, valances)
-    
+    counter_top_data_write(wb, ws_template)
     return wb_template
 
 if __name__ == "__main__":
