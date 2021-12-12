0fb4c7fa44a2a76e13896ddae3d511e1155814dd
Jeevanantham Anitha <aniththaaniththa149@gmail.com>
2021-12-12 15:42:05 +0530

    image acess file

--- Projects/image/image_acess_work.py
+++ Projects/image/image_acess_work.py
@@ -31,7 +31,13 @@ def get_file_list(root_dir,E):
     for file_path in file_list:
         imge_path = (folder_path + file_path)
         shutil.copy(imge_path, collection_images_path)
-     
+        image = Image.open(file_path)
+        exifdata = image.getexif()
+        #print(file_path)
+        for tagid in exifdata:   
+            tagname = TAGS.get(tagid, tagid)
+            value = exifdata.get(tagid)
+            print(f"{tagname:25}: {value}")
     return file_list
 
 file_list = get_file_list('./'+ image_folder_name,EXTENSIONS)
