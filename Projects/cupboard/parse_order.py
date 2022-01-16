0c6612150e03b3ee493183c823fad92c6decb0f3
Jeevanantham Anitha <aniththaaniththa149@gmail.com>
2022-01-16 17:54:10 +0530

    add framed mirror data

--- Projects/cupboard/parse_order.py
+++ Projects/cupboard/parse_order.py
@@ -1,10 +1,13 @@
 from parse_vanity_info import *
 
+
 def get_order_list(wb, cupboard_parts):
     '''
     Parse main sheet and get the order list
     '''
     ws = wb["Main Sheet"]
+    work_week = ws['H1'].value
+    print(f'WORK_WEEK {work_week}')
 
     ordered_parts = []
     for row in range(1, ws.max_row+1):
@@ -15,6 +18,7 @@ def get_order_list(wb, cupboard_parts):
             color = ws.cell(row,9).value.strip()
             parts = cupboard_parts[cupboard]
             parts_details = [ [x[0], f'{material}_{style}_{color}_{x[1]}_{x[2]}'] for x in parts ]
+            
             for new in parts_details:
                 for i, exist in enumerate(ordered_parts):
                     if (new[1] == exist[1]):
@@ -27,11 +31,42 @@ def get_order_list(wb, cupboard_parts):
         details = x[1].split('_')
         details.insert(0, x[0])
         order.append(details)
+     
 
     #Sort by material, name, size, style and color
     items = sorted(order, key=lambda item: (item[1], item[5], item[4], item[2], item[3]))
+    for item_name in items:
+        if item_name[5] == "TOWER MOULDING" or  item_name[5] == "DOOR MOULDING":
+            item_name[5] = "MOULDING"
+     
 
-    return items
+    # get framed mirror data into a list
+    framed_mirror_data = []
+    final_list = []
+    for row in range(1, ws.max_row+1):
+        cupboard = ws.cell(row,5).value
+        if(isinstance(cupboard,int)):
+            if ws.cell(row,13).value != None:
+                framed_mirror_count = re.findall('\d+',ws.cell(row+2,13).value.strip())
+                framed_mirror_list = [int(framed_mirror_count[0]),f'{ws.cell(row,8).value.strip()}_{ws.cell(row,7).value.strip()}_{ws.cell(row,9).value.strip()}_{ws.cell(row,13).value.strip()}']
+                framed_mirror_data.append(framed_mirror_list)
+                 
+    fr_set = set()
+    for List in framed_mirror_data:
+        fr_set.add(List[1])
+    
+    for data in fr_set:
+        total = 0
+        for List in framed_mirror_data:
+            if data == List[1]:
+                total += List[0]
+         
+        data = data.split('_')
+        data.insert(0, total)
+        data.insert(5, "FRAMED MIRROR")
+        items.append(data)
+    return (items, work_week)
+     
             
 
 if __name__ == "__main__":
@@ -40,10 +75,10 @@ if __name__ == "__main__":
         exit(1)
 
     excel_file = sys.argv[1]
-    wb = openpyxl.load_workbook(excel_file)
+    wb = openpyxl.load_workbook(excel_file, data_only=True)
     cupboard_parts = get_cupboard_list(wb)
-    items = get_order_list(wb, cupboard_parts)
+    items, work_week = get_order_list(wb, cupboard_parts)
 
-    for p in items:
-        print(p)
+    #for p in items:
+     #   print(p)
 
