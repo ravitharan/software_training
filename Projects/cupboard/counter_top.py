b94c9829901459bea898e0d2d0671cb49ab1848d
Jeevanantham Anitha <aniththaaniththa149@gmail.com>
2022-01-21 15:27:52 +0530

    change data view

--- Projects/cupboard/counter_top.py
+++ Projects/cupboard/counter_top.py
@@ -78,11 +78,14 @@ def write_counter_top(ws, start_row, details, sizes, colors, sink_counts):
     row = start_row
     max_col_width = [0] * (1 + len(colors))
 
+    colors.insert(0, "COUNTER TOP")
     for i, color in enumerate(colors):
-        max_col_width[i+1] = len(color)
-        ws.cell(row, i+2).value = color
-        ws.cell(row, i+2).font = Font(bold=True)
-        ws.cell(row, i+2).alignment = Alignment(horizontal="center")
+        if i < (len(colors) - 3):
+            max_col_width[i+1] = (len(color) + 3)
+            ws.cell(row, i+1).value = color
+            ws.cell(row, i+1).font = Font(bold=True)
+            ws.cell(row, i+1).alignment = Alignment(horizontal="center")
+    colors.pop(0)
     row += 1
 
     sink_splash_total = 0
@@ -91,12 +94,13 @@ def write_counter_top(ws, start_row, details, sizes, colors, sink_counts):
         max_col_width[0] = max(max_col_width[0], len(size))
         ws.cell(row, 1).value = size
         for i, color in enumerate(colors):
-            if (size in details) and (color in details[size]):
-                ws.cell(row, i+2).value = details[size][color]
-                ws.cell(row, i+2).alignment = Alignment(horizontal="center")
-                total[i] += details[size][color]
-                if color == RIGHT_SPLASH or color == LEFT_SPLASH:
-                    sink_splash_total += details[size][color]
+            if i < (len(colors) - 2):
+                if (size in details) and (color in details[size]):
+                    ws.cell(row, i+2).value = details[size][color]
+                    ws.cell(row, i+2).alignment = Alignment(horizontal="center")
+                    total[i] += details[size][color]
+                    if color == RIGHT_SPLASH or color == LEFT_SPLASH:
+                        sink_splash_total += details[size][color]
         row += 1
 
     border_style = Border(top = Side(style='double'), bottom = Side(style='thin'))
@@ -108,9 +112,6 @@ def write_counter_top(ws, start_row, details, sizes, colors, sink_counts):
             ws.cell(row, i+2).value = total[i]
             ws.cell(row, i+2).alignment = Alignment(horizontal="center")
             ws.cell(row, i+2).border = border_style
-
-    for i, col_width in enumerate(max_col_width):
-        ws.column_dimensions[get_column_letter(i+1)].width = col_width * 1.5
 
     row += 2
     ws.cell(row, 1).value = "RECTANGULAR"
@@ -125,6 +126,51 @@ def write_counter_top(ws, start_row, details, sizes, colors, sink_counts):
 
     ws.cell(row, 2).value = sink_counts[1]
     ws.cell(row, 2).alignment = Alignment(horizontal="center")
+    row += 3
+
+    colors.insert(0, "SIDE SPLASH")
+    for i, color in enumerate(colors):
+        if i < (len(colors) - 3):
+            max_col_width[i+1] = (len(color) + 3)
+            ws.cell(row, i+1).value = color
+            ws.cell(row, i+1).font = Font(bold=True)
+            ws.cell(row, i+1).alignment = Alignment(horizontal="center")
+    colors.pop(0)
+    row += 1
+
+    splash_total = 0
+    total = [0] * len(colors)
+    for key in details:
+        for value in details[key]:
+            if value == 'RIGHT SPLASH' or value == 'LEFT SPLASH':
+                max_col_width[0] = max(max_col_width[0], len(size))
+                ws.cell(row, 1).value = key + "  " + value[0:5]
+                for i, color in enumerate(colors):
+                    if i < (len(colors) - 2):
+                        for color_count in details[key]:
+                            if color == color_count:
+                                ws.cell(row,i+2).value = details[key][value]
+                                ws.cell(row, i+2).alignment = Alignment(horizontal="center")
+                                total[i] += details[key][value]        
+                row += 1
+    
+    border_style = Border(top = Side(style='double'), bottom = Side(style='thin'))
+    ws.cell(row, 1).value = sum(total) - splash_total
+    ws.cell(row, 1).alignment = Alignment(horizontal="center")
+    ws.cell(row, 1).border = border_style
+    for i, color in enumerate(colors):
+        if i < (len(colors) - 3):
+            if total[i]:
+                ws.cell(row, i+2).value = total[i]
+                ws.cell(row, i+2).alignment = Alignment(horizontal="center")
+                ws.cell(row, i+2).border = border_style
+            else:
+                ws.cell(row, i+2).alignment = Alignment(horizontal="center")
+                ws.cell(row, i+2).border = border_style
+                 
+    for i, col_width in enumerate(max_col_width):
+        ws.column_dimensions[get_column_letter(i+1)].width = col_width * 1.5
+        
     return row
 
 if __name__ == "__main__":
