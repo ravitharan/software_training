f97f4d007984c9bd03544bf539f91eb5cb3a1e73
Baskaran Tharsan <Tharshanbasss@gmail.com>
2022-01-30 11:56:32 +0530

    counter top splash should be below to color list

--- Projects/cupboard/counter_top.py
+++ Projects/cupboard/counter_top.py
@@ -16,54 +16,68 @@ def get_counter_tops(wb):
     ws = wb["Main Sheet"]
     maximum_row = ws.max_row
     counter_tops = []
+    counter_tops_sink = []
+    size_sink = []
     sizes = set()
     colors = set()
-
+    sinks = set()
+    sink_colors = set()
+    
     rect_count = 0
     oval_count = 0
     for x in range(1 , maximum_row + 1):
-        cupboard_id  = ws["E" + str(x)].value
+        order_number  = ws["AD" + str(x)].value 
         size  = ws["AF" + str(x)].value
         color = ws["AG" + str(x)].value
         sink  = ws["AH" + str(x)].value
         sink_splash = ws["Z" + str(x)].value
-        if isinstance(cupboard_id, int) and size and color and sink:
-            size  = size.strip()
-            color = color.strip()
-            sink  = sink.strip()
-            size = size.replace('"', '')
-            if sink == "RECTANGULAR":
-                rect_count += 1
-            elif sink == "OVAL":
-                oval_count += 1
-            else:
-                print(f'Invalid sink type "{sink}" at cell AH{x}')
-            counter_tops.append([size, color])
-            sizes.add(size)
-            colors.add(color)
+        if (type(order_number) == int) or bool(re.search(r'\d',str(order_number))):
+            if size != None:
+                size  = size.strip()
+                color = color.strip()
+                sink  = sink.strip()
+                size = size.replace('"', '')
+                if sink == "RECTANGULAR":
+                    rect_count += 1
+                elif sink == "OVAL":
+                    oval_count += 1
+                else:
+                    print(f'Invalid sink type "{sink}" at cell AH{x}')
+                counter_tops.append([size, color])
+                sizes.add(size)
+                colors.add(color)
 
-            if sink_splash == "R" or sink_splash == "LR":
-                counter_tops.append([size, RIGHT_SPLASH])
-            if sink_splash == "L" or sink_splash == "LR":
-                counter_tops.append([size, LEFT_SPLASH])
-
+                if sink_splash == "R" or sink_splash == "LR":
+                    counter_tops_sink.append([size,color,RIGHT_SPLASH])
+                    size_sink.append([f'{size}_{RIGHT_SPLASH}'])
+                    sink_colors.add(color)
+                    #counter_tops.append([size, RIGHT_SPLASH])
+                if sink_splash == "L" or sink_splash == "LR":
+                    counter_tops_sink.append([size, color, LEFT_SPLASH])
+                    size_sink.append([f'{size}_{LEFT_SPLASH}'])
+                    sink_colors.add(color)
+                    #counter_tops.append([size, LEFT_SPLASH])
+                 
     sizes = list(sizes)
     colors = list(colors)
+    sinks = list(sinks)
+    sink_colors = list(sink_colors)
+    
+    #sizes.sort(key = lambda x: int(x.split()[0]))
+    
+    colors.sort() 
+    #colors.append('')
+    sinks.append(RIGHT_SPLASH)
+    sinks.append(LEFT_SPLASH)
+     
+    return counter_tops, list(sizes), list(colors), [rect_count, oval_count], counter_tops_sink, size_sink, sink_colors 
 
-    sizes.sort(key = lambda x: int(x.split()[0]))
-    colors.sort()
-
-    colors.append('')
-    colors.append(RIGHT_SPLASH)
-    colors.append(LEFT_SPLASH)
-
-    return counter_tops, list(sizes), list(colors), [rect_count, oval_count]
-
-def get_countertop_details(counter_tops, sizes, colors):
+def get_countertop_details(counter_tops, sizes, colors, counter_tops_sink,size_sink):
 
     details = {}
-
+     
     for item in counter_tops:
+         
         if (item[0] in details) and (item[1] in details[item[0]]):
             details[item[0]][item[1]] += 1
         else:
@@ -71,21 +85,34 @@ def get_countertop_details(counter_tops, sizes, colors):
                 details[item[0]] = {}
             details[item[0]][item[1]] = 1
 
-    return details
+    size_sink_set = set()
+    for List in size_sink:
+        size_sink_set.add(List[0])
 
-def write_counter_top(ws, start_row, details, sizes, colors, sink_counts):
+    sink_details = {}
+    for data in size_sink_set:
+        data = data.split('_')
+        for List in counter_tops_sink:
+            if data[0] == List[0] and data[1] == List[2]:
+                key = List[0]+ '-' + List[2]
+                if (key in sink_details):
+                    sink_details[key][List[1]] += 1
+                else:
+                    sink_details[key] = {}
+                    sink_details[key][List[1]] = 1    
+     
+    return details, sink_details
+
+def write_counter_top(ws, start_row, details, sizes, colors, sink_counts, sink_colors, sink_details):
 
     row = start_row
     max_col_width = [0] * (1 + len(colors))
 
-    colors.insert(0, "COUNTER TOP")
     for i, color in enumerate(colors):
-        if i < (len(colors) - 3):
-            max_col_width[i+1] = (len(color) + 3)
-            ws.cell(row, i+1).value = color
-            ws.cell(row, i+1).font = Font(bold=True)
-            ws.cell(row, i+1).alignment = Alignment(horizontal="center")
-    colors.pop(0)
+        max_col_width[i+1] = len(color)
+        ws.cell(row, i+2).value = color
+        ws.cell(row, i+2).font = Font(bold=True)
+        ws.cell(row, i+2).alignment = Alignment(horizontal="center")
     row += 1
 
     sink_splash_total = 0
@@ -94,13 +121,12 @@ def write_counter_top(ws, start_row, details, sizes, colors, sink_counts):
         max_col_width[0] = max(max_col_width[0], len(size))
         ws.cell(row, 1).value = size
         for i, color in enumerate(colors):
-            if i < (len(colors) - 2):
-                if (size in details) and (color in details[size]):
-                    ws.cell(row, i+2).value = details[size][color]
-                    ws.cell(row, i+2).alignment = Alignment(horizontal="center")
-                    total[i] += details[size][color]
-                    if color == RIGHT_SPLASH or color == LEFT_SPLASH:
-                        sink_splash_total += details[size][color]
+            if (size in details) and (color in details[size]):
+                ws.cell(row, i+2).value = details[size][color]
+                ws.cell(row, i+2).alignment = Alignment(horizontal="center")
+                total[i] += details[size][color]
+                if color == RIGHT_SPLASH or color == LEFT_SPLASH:
+                    sink_splash_total += details[size][color]
         row += 1
 
     border_style = Border(top = Side(style='double'), bottom = Side(style='thin'))
@@ -112,6 +138,9 @@ def write_counter_top(ws, start_row, details, sizes, colors, sink_counts):
             ws.cell(row, i+2).value = total[i]
             ws.cell(row, i+2).alignment = Alignment(horizontal="center")
             ws.cell(row, i+2).border = border_style
+
+    #for i, col_width in enumerate(max_col_width):
+     #   ws.column_dimensions[get_column_letter(i+1)].width = col_width * 1.5
 
     row += 2
     ws.cell(row, 1).value = "RECTANGULAR"
@@ -126,51 +155,41 @@ def write_counter_top(ws, start_row, details, sizes, colors, sink_counts):
 
     ws.cell(row, 2).value = sink_counts[1]
     ws.cell(row, 2).alignment = Alignment(horizontal="center")
-    row += 3
+    row += 2
 
-    colors.insert(0, "SIDE SPLASH")
-    for i, color in enumerate(colors):
-        if i < (len(colors) - 3):
-            max_col_width[i+1] = (len(color) + 3)
-            ws.cell(row, i+1).value = color
-            ws.cell(row, i+1).font = Font(bold=True)
-            ws.cell(row, i+1).alignment = Alignment(horizontal="center")
-    colors.pop(0)
+    for i, color in enumerate(sink_colors):
+        max_col_width[i+1] = len(color)
+        ws.cell(row, i+2).value = color
+        ws.cell(row, i+2).font = Font(bold=True)
+        ws.cell(row, i+2).alignment = Alignment(horizontal="center")
     row += 1
-
-    splash_total = 0
-    total = [0] * len(colors)
-    for key in details:
-        for value in details[key]:
-            if value == 'RIGHT SPLASH' or value == 'LEFT SPLASH':
-                max_col_width[0] = max(max_col_width[0], len(size))
-                ws.cell(row, 1).value = key + "  " + value[0:5]
-                for i, color in enumerate(colors):
-                    if i < (len(colors) - 2):
-                        for color_count in details[key]:
-                            if color == color_count:
-                                ws.cell(row,i+2).value = details[key][value]
-                                ws.cell(row, i+2).alignment = Alignment(horizontal="center")
-                                total[i] += details[key][value]        
-                row += 1
     
+    sink_total = [0] * len(sink_colors)
+    for size_sink in sink_details:
+        max_col_width[0] = max(max_col_width[0], len(size))
+        ws.cell(row, 1).value = size_sink
+        for i, color in enumerate(sink_colors):
+            if (color in sink_details[size_sink]):    
+                ws.cell(row, i+2).value = (sink_details[size_sink][color])
+                ws.cell(row, i+2).alignment = Alignment(horizontal="center")
+                sink_total[i] += sink_details[size_sink][color]
+                 
+        row += 1
+
     border_style = Border(top = Side(style='double'), bottom = Side(style='thin'))
-    ws.cell(row, 1).value = sum(total) - splash_total
+    ws.cell(row, 1).value = sum(sink_total)
     ws.cell(row, 1).alignment = Alignment(horizontal="center")
     ws.cell(row, 1).border = border_style
-    for i, color in enumerate(colors):
-        if i < (len(colors) - 3):
-            if total[i]:
-                ws.cell(row, i+2).value = total[i]
-                ws.cell(row, i+2).alignment = Alignment(horizontal="center")
-                ws.cell(row, i+2).border = border_style
-            else:
-                ws.cell(row, i+2).alignment = Alignment(horizontal="center")
-                ws.cell(row, i+2).border = border_style
-                 
+
+    for i, color in enumerate(sink_colors):
+        if sink_total[i]:
+            ws.cell(row, i+2).value = sink_total[i]
+            ws.cell(row, i+2).alignment = Alignment(horizontal="center")
+            ws.cell(row, i+2).border = border_style
+
     for i, col_width in enumerate(max_col_width):
-        ws.column_dimensions[get_column_letter(i+1)].width = col_width * 1.5
-        
+        ws.column_dimensions[get_column_letter(i+1)].width = col_width * 1.7
+            
     return row
 
 if __name__ == "__main__":
@@ -180,13 +199,13 @@ if __name__ == "__main__":
 
     excel_file = sys.argv[1]
     wb = openpyxl.load_workbook(excel_file, data_only=True)
-    counter_tops, sizes, colors, counts = get_counter_tops(wb)
-    details =  get_countertop_details(counter_tops, sizes, colors)
+    counter_tops, sizes, colors, counts, counter_tops_sink, size_sink, sink_colors = get_counter_tops(wb)
+    details, sink_details =  get_countertop_details(counter_tops, sizes, colors, counter_tops_sink, size_sink)
 
     wb = openpyxl.Workbook()
     ws = wb.active
     ws.title = SHEET_COUNTER_TOP
 
-    write_counter_top(ws, 3, details, sizes, colors, counts)
+    write_counter_top(ws, 3, details, sizes, colors, counts, sink_colors, sink_details)
 
     wb.save('output.xlsx')
